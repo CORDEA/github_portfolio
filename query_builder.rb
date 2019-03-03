0b8972bf423b5a39d0171bd76ef3f6b48d0edb0b
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-03 14:19:31 +0900

    add query builder

---
+++ query_builder.rb
@@ -0,0 +1,19 @@
+class QueryBuilder
+  def initialize(repo, author, created)
+    @repo = repo
+    @author = author
+    @created = created
+  end
+
+  def pr_query
+    "is:pr repo:#{@repo} author:#{@author} created:#{@created}"
+  end
+
+  def reviewed_pr_query
+    "is:pr repo:#{@repo} reviewed-by:#{@author} -author:#{@author} created:#{@created}"
+  end
+
+  def issue_query
+    "is:issue repo:#{@repo} author:#{@author} created:#{@created}"
+  end
+end
