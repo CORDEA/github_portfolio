f9fb4f603cd66bdb8c9afa2d3318e0d7c01a36b3
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-03 17:44:39 +0900

    define client

---
+++ client.rb
@@ -0,0 +1,42 @@
+require './reviewed_prs'
+
+class Client
+
+  def initialize(client, builder)
+    @client = client
+    @builder = builder
+  end
+
+  def fetch_prs(repo)
+    prs = @client.search_issues(@builder.pr_query)
+    pr_numbers = prs.items.map {|pr| pr.number}
+    pr_numbers.map {|number| @client.pull_request(repo, number)}
+  end
+
+  def fetch_reviewed_prs(repo, author)
+    prs = @client.search_issues(@builder.reviewed_pr_query)
+    pr_numbers = prs.items.map {|pr| pr.number}
+    pr_details = pr_numbers.map {|number| @client.pull_request(repo, number)}
+    reviewed_comments = fetch_reviewed_comments(repo, pr_numbers, author)
+    comments = fetch_comments(repo, pr_numbers, author)
+    ReviewedPullRequests.new(pr_details, reviewed_comments, comments)
+  end
+
+  def fetch_issues
+    @client.search_issues(@builder.issue_query).items
+  end
+
+  private
+
+  def fetch_reviewed_comments(repo, numbers, author)
+    numbers
+        .flat_map {|number| @client.pull_request_comments(repo, number)}
+        .select {|issue| issue.user.login == author}
+  end
+
+  def fetch_comments(repo, numbers, author)
+    numbers
+        .flat_map {|number| @client.issue_comments(repo, number)}
+        .select {|issue| issue.user.login == author}
+  end
+end
